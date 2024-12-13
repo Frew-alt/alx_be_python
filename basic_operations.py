9d2172e8b3dba750f3890b740129a61a4795884a
Frew-alt <ntwg2022@gmail.com>
2024-12-13 13:18:20 +0300

    basic_operations.py

---
+++ basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+sum_result = number1 + number2
+difffrence_result = number1 - number2
+product_result = number1 * number2
+print("Sum of", number1, "and", number2, "is", sum_result)
+print("diffrence of", number1, "and", number2, "is", difffrence_result)
+print("product of", number1, "and", number2, "is", product_result)
